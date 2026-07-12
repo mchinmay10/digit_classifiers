e29fec9dad78700ddfca92656e8904223ceec70b
novice0 <chinmaymulmule@gmail.com>
2026-07-12 11:41:52 +0530

    added test function for forward pass of 10 neurons

--- dig_class_01/scripts/layers.py
+++ dig_class_01/scripts/layers.py
@@ -1,4 +1,5 @@
 import time
+import random
 from vector import dot
 from activations import step, sigmoid, relu
 
@@ -23,6 +24,10 @@ class Neuron:
             return f"Invalid input {x}"
 
 
+class DenseLayer:
+    pass
+
+
 def forward_test():
     print(f"Executing test cases for forward function...")
     time.sleep(3)
@@ -31,9 +36,25 @@ def forward_test():
 
 
 def ten_neuron_fwd_test():
-    pass
+    print("Executing test cases for ten neurons accepting the same input...")
+    time.sleep(3)
+    input = [0.32, 0.55]
+    w = []
+    b = []
+    print(f"Input: {input}")
+    for i in range(10):
+        w_i = [round(random.random(), 2) for _ in range(2)]
+        b_i = round(random.random(), 2)
+        n_i = Neuron(w_i, b_i)
+        f_i = n_i.forward(input)
+        print(f"----Printing details of Neuron {i}----")
+        time.sleep(2)
+        print(f"Weights: {w_i}")
+        print(f"Bias: {b_i}")
+        print(f"Forward pass: {f_i:.2f}")
 
 
 if __name__ == "__main__":
     print(f"----Running test cases for the layers.py file----")
     forward_test()
+    ten_neuron_fwd_test()
