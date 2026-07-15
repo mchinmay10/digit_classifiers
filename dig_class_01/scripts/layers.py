ca1b3f79c8fffdf1e56f32bf746fa0a2bc405728
novice0 <chinmaymulmule@gmail.com>
2026-07-15 15:17:23 +0530

    added simulation for temporary weight change

--- dig_class_01/scripts/layers.py
+++ dig_class_01/scripts/layers.py
@@ -59,11 +59,11 @@ class DenseLayer_v2:
             self.neurons.append(Neuron(weight_matrix[i], bias))
 
     def layer_forward(self, x):
-        output = []
+        activ_output = []
         for neuron in self.neurons:
-            output.append(neuron.forward(x))
+            activ_output.append(neuron.forward(x))
 
-        return output
+        return activ_output
 
     def layer_forward_debug(self, x):
         print(f"Input vector: {x}")
@@ -147,10 +147,134 @@ def manual_optimisation():
                     )
             print("Update bias...")
             time.sleep(1)
-            bias = float(input("Enter bias term: "))
+            bias = get_bias_from_user()
         else:
             print(f"Quitting manual optimisation...")
             time.sleep(1)
+
+
+# helper function that temperorily changes one weight, computes loss and resores the original weight
+def temp_weight_change(
+    nw_input: list, layer: DenseLayer_v2, index: tuple, delta: float, target: list
+):
+    i, j = index
+    loss_list = []
+    activ_output = layer.layer_forward(nw_input)
+    loss = mean_squared_error(target, activ_output)
+    loss_list.append(loss)
+    print(f"Inital / Previous Loss: {loss}")
+    time.sleep(2)
+    print(f"Temporarily changing weight at index: {index} by {delta}")
+    save_weight = layer.weight_matrix[i][j]
+    new_weight = save_weight + delta
+    layer.weight_matrix[i][j] = new_weight
+    time.sleep(2)
+    print(f"Value of old weight: {save_weight}")
+    print(f"Value of new weight: {new_weight}")
+    activ_output = layer.layer_forward(nw_input)
+    loss = mean_squared_error(target, activ_output)
+    loss_list.append(loss)
+    time.sleep(2)
+    print(f"Loss: {loss}")
+    time.sleep(2)
+    if loss_list[0] > loss_list[1]:
+        print(f"Loss decreased from {loss_list[0]} -> {loss_list[1]}")
+    elif loss_list[0] < loss_list[1]:
+        print(f"Loss increased from {loss_list[0]} -> {loss_list[1]}")
+    else:
+        print("Loss unchanged")
+    time.sleep(2)
+    print("Restoring weight...")
+    time.sleep(2)
+    layer.weight_matrix[i][j] = save_weight
+
+
+# function to recieve weights from user (mainly for manual optimisation and initialisation purpose)
+def get_weights_from_user(num_neurons, input_len):
+    weight_matrix = [[0.0 for _ in range(input_len)] for _ in range(num_neurons)]
+    print("Enter weight matrix")
+    time.sleep(3)
+    for i in range(num_neurons):
+        for j in range(input_len):
+            weight_matrix[i][j] = float(
+                input(f"Value of w at: row {i} and column {j}: ")
+            )
+
+    return weight_matrix
+
+
+# function to recieve bias term from user
+def get_bias_from_user():
+    bias = float(input("Enter bias term: "))
+    return bias
+
+
+# function that gets dummy target from user for purpose of manual simulation of learning process
+def get_dummy_targets_from_user(n):
+    targets = []
+    for i in range(n):
+        target = float(input(f"Enter target {i + 1}: "))
+        targets.append(target)
+
+    return targets
+
+
+# function to get index of the weight to be changed temporarily
+# to add bounding check on the row and column that we accept from user
+def get_weight_index_from_user():
+    weight_index = []
+    for i in range(2):
+        if i == 0:
+            row = int(input("Enter row index: "))
+            weight_index.append(row)
+        else:
+            col = int(input("Enter column index: "))
+            weight_index.append(col)
+
+    return tuple(weight_index)
+
+
+# get input to network from user
+def get_nw_input_from_user():
+    nw_input = []
+    n = int(input("Enter number of inputs: "))
+    for i in range(n):
+        single_input = float(input(f"Enter input {i + 1}: "))
+        nw_input.append(single_input)
+
+    return nw_input
+
+
+# function that gracefully runs the temporary weight change simulation
+def simulate_temp_weight_change():
+    nw_input = get_nw_input_from_user()
+    time.sleep(2)
+    num_neurons = int(input("Enter number of neurons in the layer: "))
+    time.sleep(2)
+    weigth_matrix = get_weights_from_user(num_neurons, len(nw_input))
+    time.sleep(2)
+    bias = get_bias_from_user()
+    time.sleep(2)
+    l = DenseLayer_v2(
+        num_neurons,
+        weigth_matrix,
+        bias,
+    )
+    target = get_dummy_targets_from_user(num_neurons)
+    time.sleep(2)
+    CONTINUE = 1
+    while CONTINUE == 1:
+        CONTINUE = int(input("Continue manual temperory weight change? [0/1]: "))
+        if CONTINUE == 1:
+            index = get_weight_index_from_user()
+            time.sleep(2)
+            delta = float(input("Enter delta for weight change: "))
+            time.sleep(2)
+            temp_weight_change(nw_input, l, index, delta, target)
+            clear_screen()
+        else:
+            print("Quitting weight temperory change simulation...")
+            time.sleep(2)
 
 
 # Test cases:
@@ -231,4 +355,5 @@ if __name__ == "__main__":
     # dense_layer_v2_forward_test()
     # predict_digit_test()
     # simulate_fwd_pass()
-    manual_optimisation()
+    # manual_optimisation()
+    simulate_temp_weight_change()
