dd2b24244a3919cec10b82b04b36fad880571748
novice0 <chinmaymulmule@gmail.com>
2026-08-22 00:59:19 +0530

    added first version of border print function

--- dig_class_01/scripts/visual.py
+++ dig_class_01/scripts/visual.py
@@ -12,7 +12,25 @@ def load_print(statement: str):
     time.sleep(2)
 
 
+def border_print_v1(statement: str):
+    top_border = "⎽" * len(statement)
+    bottom_border = "‾" * len(statement)
+    time.sleep(1)
+    print(f"{top_border}\n{statement}\n{bottom_border}")
+    time.sleep(1)
+
+
 def function_header(statement: str):
     time.sleep(1)
     print(f"----{statement}----")
     time.sleep(1)
+
+
+# Test cases
+def border_print_v1_test():
+    function_header("Test cases for the border_print_v1 function")
+    border_print_v1("Hello World")
+
+
+if __name__ == "__main__":
+    border_print_v1_test()
